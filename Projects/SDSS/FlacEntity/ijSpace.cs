58ddf7842d608e0dfdf1ec142eee95690aa28ee2
619086871 <619086871@qq.com>
2016-12-25 17:20:05 +0800

    Add project files.

---
+++ Projects/SDSS/FlacEntity/ijSpace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDSS.FlacEntity
+{
+    /// <summary>
+    /// Flac 中用来进行定位的ij坐标系
+    /// </summary>
+    public class ijSpace
+    {
+
+    }
+}
---
+++ Projects/SDSS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDSS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            test();
+            return;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        private static void test()
+        {
+            string[] ss = new string[5] { "1", "1.5", "", "", "" };
+            var tt = ss.Where(r => !string.IsNullOrEmpty(r)).Select(Convert.ToSingle).ToArray();
+            var c = Color.FromArgb(1,Color.Pink);
+            
+            var t = ss.Select(Convert.ToSingle).ToArray();
+        }
+    }
+}
